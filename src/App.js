8fb696f43c72c758a19b0aa1dc15f092d6e20c2d
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-06 23:57:33 +0600

    NavLink , outlet added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,  RouterProvider } from 'react-router-dom'
 import About from './components/About'
 import Books from './components/Books'
-import Header from './components/Header'
+import ErrorPage from './components/ErrorPage'
 import Home from './components/Home'
+import Main from './Main/Main'
 
 function App() {
   const router= createBrowserRouter([
     {
       path:'/',
-      element: <Home></Home>
-    },
-    {
-      path:'home',
-      element: <Home></Home>
+      element: <Main></Main>, 
+      errorElement: <ErrorPage></ErrorPage>,
+      children:[
+        {
+          path:'home',
+          element: <Home></Home>
+        },
+        {
+          path:'books',
+          element: <Books></Books>
+        },
+        {
+          path:'about',
+          element: <About></About>
+        }
+      ]
     }
-    ,
-    {
-      path:'books',
-      element: <Books></Books>
-    }
-    ,
-    {
-      path:'about',
-      element: <About></About>
-    }
+    
   ])
  return(
   <div>
     
-<Header></Header>
 <RouterProvider router={router}></RouterProvider>
   </div>
 
--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import Header from './Header'
+import { useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
+  const error = useRouteError();
   return (
     <>
-      <Header />
+     
       <div className='flex flex-col min-h-[700px] justify-center items-center'>
         <h1 className='text-4xl'>Ops! An Error Ocurred!</h1>
+        <div>
+          <p className='text-red-600 font-semibold text-2xl'>{error.statusText || error.message}</p>
+          <p className='text-red-600 font-semibold text-2xl'>{error.status}</p>
+        </div>
       </div>
     </>
   )
